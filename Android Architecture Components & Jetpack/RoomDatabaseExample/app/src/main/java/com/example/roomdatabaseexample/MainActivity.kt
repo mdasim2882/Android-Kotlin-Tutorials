09fcd182b48280bae6fd7e08878cef7ce2740966
mdasim2882 <mohdasim2882@gmail.com>
2023-04-04 01:49:19 +0530

    Optimized imports in MainActivity.kt

--- Android Architecture Components & Jetpack/RoomDatabaseExample/app/src/main/java/com/example/roomdatabaseexample/MainActivity.kt
+++ Android Architecture Components & Jetpack/RoomDatabaseExample/app/src/main/java/com/example/roomdatabaseexample/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.example.roomdatabaseexample
 
-import android.database.sqlite.SQLiteQuery
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.room.Room
-import androidx.room.Update
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import java.util.*
