b012dc038eb0c58695fc6cbdc478f771a688f2d0
mdasim2882 <mohdasim2882@gmail.com>
2023-09-26 02:59:34 +0530

    added dependencies and setup architecture

---
+++ Android Architecture Components & Jetpack/MVVMSampleApp/app/src/main/java/com/example/samplemvvmapp/ui/auth/AuthViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.samplemvvmapp.ui.auth
+
+import android.view.View
+import androidx.lifecycle.ViewModel
+
+class AuthViewModel : ViewModel() {
+
+    var email: String? = null
+    var password: String? = null
+
+    fun onLoginButtonClick(v: View)
+    {
+        if( email.isNullOrEmpty() || password.isNullOrEmpty())
+        {
+            // Ask again for input
+            return
+        }else{
+            // TODO: Authentication logic
+        }
+    }
+
+
+
+
+}
+
+
